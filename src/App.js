47df7f3e9d3a9a6cc0fec3576d6fa473c17aa55c
Elizabeth Asbell <e.asbell8@gmail.com>
2019-02-13 14:02:56 -0700

    Append smoothie names to dom.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import {smoothies, ingredients} from './smoothie-data.js'
+import SmoothieContainer from './SmoothieContainer.js'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      smoothies: smoothies,
+      ingredients: ingredients
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Blend It!</h1>
         <h2>Choose Your Ingredients</h2>
+        <SmoothieContainer smoothies={this.state.smoothies}/>
       </div>
     );
   }
