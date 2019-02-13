a7b823cc30d66f32800723e1c84e0c774028dfe7
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-02-13 14:28:12 -0700

    Append ingredient names to dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {smoothies, ingredients} from './smoothie-data.js'
 import SmoothieContainer from './SmoothieContainer.js'
+import IngredientContainer from './IngredientContainer.js'
 import './App.css';
 
 class App extends Component {
@@ -18,6 +19,7 @@ class App extends Component {
       <div className="App">
         <h1>Blend It!</h1>
         <h2>Choose Your Ingredients</h2>
+        <IngredientContainer ingredients={this.state.ingredients}/>
         <SmoothieContainer smoothies={this.state.smoothies}/>
       </div>
     );
--- src/IngredientContainer.js
+++ src/IngredientContainer.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 
 // Ingredient selection at start
-export class Ingredients extends Component {
+export default class Ingredients extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: Object.keys(this.props.ingredients)
+    }
+    console.log("ingContainer props", this.state.categories)
+  }
   render() {
     return (
       <div>
-        
+        {
+          this.state.categories.map(category => {
+            return <h3>{category}</h3>
+          })
+        }
       </div>
     )
   }
 }
 
-export default Ingredients
