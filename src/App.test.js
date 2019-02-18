15ecf77c431c07ec2d98e6c1da0e21db245192be
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-02-18 16:41:32 -0700

    Start app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,95 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+
+const mockSmoothies = [{
+id: 1,
+name: "Babe Ruth Strawberry Pineapple Banana Smoothie",
+ingredients: [
+"strawberries",
+"pineapple",
+"banana",
+"orange juice",
+"yogurt",
+"spinach",
+"chia seeds",
+"flaxseed"
+],
+recipe: [
+"1 cup strawberries",
+"1/2 cup pineapple",
+"1 banana",
+"2 cups orange juice",
+"1/2 cup greek yogurt",
+"1 cup spinach, optional",
+"1 tablespoon chia or flaxseed, optional",
+"ice"
+],
+servings: 4,
+source: "https://www.modernhoney.com/6-healthy-superfood-smoothies/",
+img: "images/babe-ruth.jpg"
+},
+{
+id: 2,
+name: "Cherry Almond Smoothie",
+ingredients: [
+"milk",
+"cherries",
+"raw almonds",
+"honey",
+"cinnamon",
+"almond extract",
+"coconut oil"
+],
+recipe: [
+"1 cup almond milk",
+"1 cup cherries",
+"3 tablespoons raw almonds",
+"2 tablespoons raw honey",
+"1 teaspoon cinnamon",
+"1/2 teaspoon almond extract",
+"1 tablespoon coconut oil"
+],
+servings: 1,
+source: "https://bakedbree.com/cherry-almond-smoothies",
+img: "images/cherry-almond.jpg"
+}];
+
+const mockIngredients = {
+  fruit: [
+  "banana",
+  "pineapple",
+  "mango"
+  ],
+  vegetables: [
+  "spinach",
+  "kale",
+  "carrot"
+  ],
+  bases: [
+  "milk",
+  "yogurt",
+  "coconut water"
+  ],
+  extras: [
+  "chia seeds",
+  "ginger",
+  "cinnamon"
+  ]
+}
+
+describe('App', () => {
+  const wrapper = shallow(
+    <App />
+  );
+
+  it('should match a snapshot with all data passed in', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
