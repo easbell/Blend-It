c6008277c4a8f1c8237afc5ff742af2a7c1babaa
Elizabeth Asbell <e.asbell8@gmail.com>
2019-02-18 09:03:06 -0700

    Toggle show specific smoothie.

--- src/Smoothie.js
+++ src/Smoothie.js
@@ -1,15 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Smoothie.css';
 
 export default class Smoothie extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      isSelected: false
+    }
   }
 
+  expandRecipe = () => {
+    const {isSelected} = this.state
+    this.setState({isSelected: !isSelected})
+    this.props.showRecipe(this.props)
+    console.log(this.props.recipe)
+  }
+  
   render = () => {
+    const {isSelected} = this.state
     return (
       <div>
-        {this.props.name}
-        <img src={this.props.img}/>
+        {!isSelected && 
+          <div className='smoothie'onClick={this.expandRecipe}>
+            <h3>{this.props.name}</h3>
+            <img src={this.props.img}/>
+          </div>
+        }
+        {isSelected && 
+          <div className='smoothie'>
+            <h3>{this.props.name}</h3>
+            <img src={this.props.img}/>
+          </div>
+        }
       </div>
     )
   }
--- src/SmoothieContainer.js
+++ src/SmoothieContainer.js
@@ -1,20 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Smoothie from './Smoothie.js'
 
-export default function SmoothieContainer(props) {
-  return (
-    <div>
-      {
-      props.smoothies.map(smoothie => {
-        return (
+export default class SmoothieContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showRecipe: false,
+      chosenSmoothie: ''
+    }
+  }
+
+  showRecipe = (smoothie) => {
+    const {showRecipe, chosenSmoothie} = this.state;
+    this.setState({showRecipe: !showRecipe, chosenSmoothie: smoothie.id})
+  }
+
+  render = () => {
+    const {showRecipe} = this.state;
+    const {chosenSmoothie} = this.state;
+    let smoothieRecipe = this.props.smoothies.find(smoothie => {
+      return smoothie.id === chosenSmoothie
+    })
+    return (
+      <div>
+        {!showRecipe && 
+          this.props.smoothies.map(smoothie => {
+            return (
+              <Smoothie
+              showRecipe={this.showRecipe}
+              name={smoothie.name}
+              img={smoothie.img}
+              recipe={smoothie.recipe}
+              key={smoothie.id}
+              id={smoothie.id}
+              />
+              )
+            })
+          }
+        {showRecipe && 
           <Smoothie 
-            name={smoothie.name}
-            img={smoothie.img}
-            key={smoothie.id}
+            showRecipe={this.showRecipe}
+            name={smoothieRecipe.name}
+            img={smoothieRecipe.img}
+            recipe={smoothieRecipe.recipe}
+            key={smoothieRecipe.id}
+            id={smoothieRecipe.id}
           />
-        )
-      })
-      }
-    </div>
-  )
+        }
+      </div>
+    )
+  }
 }
